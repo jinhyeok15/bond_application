9b85a989eb593888078702ab52fe9ebed28db822
jinhyeok15 <ekfrhd97@gmail.com>
2021-11-23 22:03:26 +0900

    [11.23] bondapp/return.py _get_soup함수 추가

--- app/src/bondproject/bondapp/return.py
+++ app/src/bondproject/bondapp/return.py
@@ -2,41 +2,42 @@ import requests
 from bs4 import BeautifulSoup
 
 _url = 'https://www.kisrating.co.kr/ratingsStatistics/statics_spread.do'
-_response = requests.get(_url)
 
 def return_by_rating(search_title):
     key_data = [
         '구분', '3m', '6m', '9m', '1y', '1y6m', '2y', '3y', '5y'
     ]
 
-    if _response.status_code == 200:
-        html = _response.text
-        soup = BeautifulSoup(html, 'html.parser')
+    soup = _get_soup(_url)
+    date = get_date()
+    tbody = soup.select_one('tbody')
+    tables = tbody.select('tr')
+    titles = tbody.select('tr > td.fc_blue_dk')
 
-        date = get_date()
-        tbody = soup.select_one('tbody')
-        tables = tbody.select('tr')
-        titles = tbody.select('tr > td.fc_blue_dk')
-
-        dct_data = {'date': date}
-        i=0
-        for title in titles:
-            if title.get_text()==search_title:
-                j=0
-                for key in key_data:
-                    dct_data[key] = tables[i].select('td')[j].get_text()
-                    j+=1
-            i+=1
-        return dct_data
-
-    else:
-        print(_response.status_code)
+    dct_data = {'date': date}
+    i=0
+    for title in titles:
+        if title.get_text()==search_title:
+            j=0
+            for key in key_data:
+                dct_data[key] = tables[i].select('td')[j].get_text()
+                j+=1
+        i+=1
+    return dct_data
 
 
 def get_date():
-    html = _response.text
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = _get_soup(_url)
     return soup.find('input', {'title':'기간선택'}).get('value')
+
+
+def _get_soup(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        html = response.text
+        return BeautifulSoup(html, 'html.parser')
+    else:
+        print(response.status_code)
 
 
 if __name__=='__main__':
