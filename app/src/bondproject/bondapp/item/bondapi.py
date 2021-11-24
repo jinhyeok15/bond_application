88c953fa79b0305dab6027e9c13b5de5560d902c
jinhyeok15 <ekfrhd97@gmail.com>
2021-11-24 17:18:28 +0900

    [11.24] get_data json불러오는 부분 함수로 분리 -> _get_json_data

--- app/src/bondproject/bondapp/item/bondapi.py
+++ app/src/bondproject/bondapp/item/bondapi.py
@@ -5,35 +5,10 @@ import json
 from datetime import datetime, timedelta
 
 
-def get_data(_max):
-    key = OPEN_API_KEY.encode()
-
-    # 현재 시간이 만일 00시-16시이면 어제 시간을 가져옴
-    now_date = datetime.now().strftime('%Y%m%d')
-    if 0<=int(_get_time())<=16:
-        now_date = _get_yst(now_date)
-    
-    queryParams = '?' + urlencode({
-        quote_plus('serviceKey') : key,
-        quote_plus('pageNo') : 1,
-        quote_plus('numOfRows') : _max,
-        quote_plus('resultType') : 'json',
-        quote_plus('basDt') : now_date
-    })
-    
-    response = urlopen(url+queryParams)
-    json_api = response.read().decode("utf-8")
-    try:
-        json_file = json.loads(json_api)
-    except JSONDecodeError:
-        print('SERVICE ERROR: http error')
-        return
-    item_list = json_file['response']['body']['items']['item']
+def get_data(page, _max):
+    item_list = _get_json_data(page, _max)
     use_data_list = []
     for item in item_list:
-        if item['scrsItmsKcdNm']!='국채':  # 국채만 가져올 것
-            continue
-
         use_data = dict()
         for name in COLUMN_NAMES:
             use_data[name] = item[name]
@@ -50,6 +25,31 @@ def _get_yst(str_date):  # 문자열 날짜 ex) '20180921'
 
 def _get_time():  # 현재 시간
     return datetime.now().strftime('%H')
+
+
+def _get_json_data(page, _max):
+    key = OPEN_API_KEY.encode()
+
+    # 현재 시간이 만일 00시-16시이면 어제 시간을 가져옴
+    now_date = datetime.now().strftime('%Y%m%d')
+    if 0<=int(_get_time())<=16:
+        now_date = _get_yst(now_date)
+    
+    queryParams = '?' + urlencode({
+        quote_plus('serviceKey') : key,
+        quote_plus('pageNo') : page,
+        quote_plus('numOfRows') : _max,
+        quote_plus('resultType') : 'json',
+        quote_plus('basDt') : now_date
+    })
+    
+    response = urlopen(url+queryParams)
+    json_api = response.read().decode("utf-8")
+    try:
+        json_file = json.loads(json_api)
+        return json_file['response']['body']['items']['item']
+    except JSONDecodeError:
+        print('SERVICE ERROR: http error')
 
 
 COLUMN_NAMES = [
@@ -69,7 +69,7 @@ if __name__ == '__main__':
     import pandas as pd
     from pandas.io.json import json_normalize 
     # 데이터프레임 타입으로 변환하기
-    org_data = get_data(30)
+    org_data = get_data(30, 30)
 
     dct_data = dict()
     for name in COLUMN_NAMES:
