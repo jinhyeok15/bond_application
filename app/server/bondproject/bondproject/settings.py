5feac2dcb9a42dcd19f33cf387b2c9b748833e07
jinhyeok15 <ekfrhd97@gmail.com>
2021-11-28 15:05:35 +0900

    [11.28] settings파일 수정 및 mysqlclient 설치 반영

--- app/server/bondproject/bondproject/settings.py
+++ app/server/bondproject/bondproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import configs
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1cs@1g4l=&+d@0z=+&x#k^q52)hrr*k2^e136!&=681exo&d$!'
+SECRET_KEY = configs.DJANGO_SECRET_KEY['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,10 +75,7 @@ WSGI_APPLICATION = 'bondproject.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': configs.MYSQL_DATABASE['default'],
 }
 
 
@@ -105,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
